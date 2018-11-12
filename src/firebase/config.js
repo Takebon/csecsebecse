a647dc747f4bdd7258f2ea5f747a4cd96ce83f8f
Takebon <37710007+Takebon@users.noreply.github.com>
2018-11-12 14:21:10 +0100

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,10 @@
 export default {
   
-    apiKey: "AIzaSyA_wO5E0YxSeyxMfSDdQrvaGhnwjKv8m7g",
-    authDomain: "csecsebecsekonyvesblog.firebaseapp.com",
-    databaseURL: "https://csecsebecsekonyvesblog.firebaseio.com",
-    projectId: "csecsebecsekonyvesblog",
-    storageBucket: "csecsebecsekonyvesblog.appspot.com",
-    messagingSenderId: "390883060045"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: 
   
 }
